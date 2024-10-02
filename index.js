00fc0e9a68f14cebf14e010efa642cd8261cffed
Nishand-RM <nishandrm9@gmail.com>
2024-10-02 22:59:44 +0530

    updated backend

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const alertRoutes = require('./routes/alerts');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 
 app.use(bodyParser.json());
@@ -17,10 +16,10 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("Connected to the MongoDB database");
 
     // start the server by listening on a port for incoming requests
-    app.listen(PORT, () => {
+    app.listen(5000, () => {
         console.log("Server is running on http://localhost:5000");
     });
-})
+})  
 .catch((err) => {
     console.log("Error connecting to the MongoDB database", err);
 });
