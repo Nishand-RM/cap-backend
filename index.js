d7f01556d1c4ecdff68613e133e8779a778f69ed
Nishand-RM <nishandrm9@gmail.com>
2024-10-05 12:38:55 +0530

    updated corp code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// backend/index.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -13,24 +11,23 @@ dotenv.config();
 const app = express();
 
 // Middleware
-
 app.use(express.json());
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());;
+// Process allowed origins from environment
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());
 
 app.use(cors({
     origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
+        if (!origin) return callback(null, true); // allow requests with no origin (e.g., mobile apps)
         if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-          return callback(new Error(msg), false);
+            const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
+            return callback(new Error(msg), false);
         }
         return callback(null, true);
-      },
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }));
-    
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -41,13 +38,11 @@ mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('✅ MongoDB connected');
-        app.listen(5000, () => {
-            console.log(`Server running on port: 5000`);
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server running on port: ${process.env.PORT || 5000}`);
         });
         scheduleAlerts();
     })
     .catch((err) => {
         console.error('❌ MongoDB connection error:', err);
     });
-
-
